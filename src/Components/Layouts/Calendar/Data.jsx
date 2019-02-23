2f407892af26afd70f7cadfdf0a8f5500d92b06b
Jon Manock <jonmanock@Jons-MacBook-Pro.local>
2019-02-23 05:01:50 -0500

    working on new events

--- src/Components/Layouts/Calendar/Data.jsx
+++ src/Components/Layouts/Calendar/Data.jsx
@@ -32,7 +32,13 @@ export default{
 
       {start:'2019-03-10', end:'2019-03-10', title:"Central Florida's 2nd Annual Wounded Vet Run", description:'Benefiting Wounded Veteran: PATRICK WICKENS', link:'https://www.facebook.com/photo.php?fbid=575015412967045&set=a.133906170411307&type=3&theater', time:'12:00pm', where:'Horsepower Ranch'},
 
-      {start:'2019-09-21', end:'2019-09-21', title:'15th Annuall USO Motorcycle Benefit Ride', link:'https://www.facebook.com/events/272753123581814/', description:'To show the men and women in our Armed Forces that the Central Florida Motorcycle Community has not forgotten their sacrifce and thank them for the job they do by supporting the USO.', where:'Orlando Harley-Davidson South', time:'8:30am - 3:00pm'}
+      {start:'2019-09-21', end:'2019-09-21', title:'15th Annuall USO Motorcycle Benefit Ride', link:'https://www.facebook.com/events/272753123581814/', description:'To show the men and women in our Armed Forces that the Central Florida Motorcycle Community has not forgotten their sacrifce and thank them for the job they do by supporting the USO.', where:'Orlando Harley-Davidson South', time:'8:30am - 3:00pm'},
+
+      {start:'2019-04-26', end:'2019-04-28', title:'Leesburg Bikefest', link:'https://www.facebook.com/events/288198628522699/', description:"The World's Largest 3-day Motorcycle & Music Event!", where:'Downtown Lessburg', time:'6:00pm - 12:00am'},
+
+      {start:'2019-02-24', end:'2019-02-24', title:'Falcons Fury Harley-Davidson', link:'https://www.facebook.com/falconsfuryhd/', description:'Join us for our 5th annual Iron Elite Day and Bike Show: $2000 in cash and prizes! Best Custom Bike - $500, Best Sound - $500, Best Vintage Bike - $500, Best Club Participation - $500', time:'11:00am', where:'Falcons Fury Harley-Davidson'},
+
+
     ];
     const events = eventMeta.map((data) =>{
       return{
@@ -79,7 +85,9 @@ export default{
 
       {start:'2019-02-24', end:'2019-02-24', title:'"Goodvibes" Cars for Cancer Charity Meet', description:'Local car enthusiast & founder of Dub Empire has been battling cancer for the past few years', where:'Castle Church Brewing', time:'1pm - 6pm', link:'https://www.facebook.com/events/458386924699589/'},
 
-      {start:'2019-04-06', end:'2019-04-06', title:'Vettes for Vets - Charity Corvette Night', description:'Car show open to ALL makes and Models, Motorcycles, Classic Cars, Hot Rods... come show off your wheels!', where:'Castle Church Brewing', time:'12pm - 12am', link:'https://www.facebook.com/events/1950110891768624/'}
+      {start:'2019-04-06', end:'2019-04-06', title:'Vettes for Vets - Charity Corvette Night', description:'Car show open to ALL makes and Models, Motorcycles, Classic Cars, Hot Rods... come show off your wheels!', where:'Castle Church Brewing', time:'12pm - 12am', link:'https://www.facebook.com/events/1950110891768624/'},
+
+      {start:'2019-03-27', end:'2019-03-27', title:'Coming Together at Mercedes Benz of Orlando', description:'A fundraising event at Mercedes Benz of Orlando in coordination with 98.9 WMMO to raise funds for the The Lifeboat Project to help provide housing, support services, and other efforts against human trafficking.', where:'Mercedes Benz of Orlando', time:'6:00pm - 8:00pm', link:'https://www.facebook.com/events/361604854424265/'}
     ];
     const events = eventMeta.map((data) =>{
       return{
