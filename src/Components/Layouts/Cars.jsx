cff5551ca50112f2f628f3ef26ad8299fe25f430
Jon Manock <jonmanock@Jons-MacBook-Pro.local>
2019-02-26 10:39:34 -0500

    redoing cars page

--- src/Components/Layouts/Cars.jsx
+++ src/Components/Layouts/Cars.jsx
@@ -4,57 +4,14 @@ import CalendarCars from './Calendar/CalendarCars';
 const Cars = () =>{
   return(
     <div className='container-fluid'>
-      <h1 className='text-center' style={{marginTop:100,color:'#fff'}}>CarFeverTV</h1>
-      <div className='card'>
-        <div className='card-body'>
-          <p className='card-text text-center'>This is the place for all things Car's</p>
-          <p className='card-text text-center'><a href='mailto:cftv@email.com'>Have a Car Event, Meetup, Rally let us know and we will add it to the calendar!</a></p>
-        </div>
-      </div>
-      <CalendarCars />
-      <div style={{marginTop:50, marginBottom:50}}>
-        <h3 className='text-center' style={{color:'#fff'}}>Past Shows</h3>
-
-        <div className='row'>
-
-          <div className='col-sm'>
-            <div className='card'>
-              <div className='card-body'>
-                <h5 className='card-title text-center'>2-18-2019</h5>
-                <div className='embed-responsive embed-responsive-21by9'>
-                  <iframe src='https://www.youtube.com/embed/UxwGlBtb4ik' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowFullScreen title='one10'></iframe>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div className='col-sm'>
-            <div className='card'>
-              <div className='card-body'>
-                <h5 className='card-title text-center'>1-21-2019</h5>
-                <div className='embed-responsive embed-responsive-21by9'>
-                  <iframe src='https://www.youtube.com/embed/dNZjsk7Fkxw' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowFullScreen title='one21'></iframe>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div className='col-sm'>
-            <div className='card'>
-              <div className='card-body'>
-                <h5 className='card-title text-center'>12-17-2018</h5>
-                <div className='embed-responsive embed-responsive-21by9'>
-                  <iframe src='https://www.youtube.com/embed/J6FWUX1XlJA' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowFullScreen title='twelve17'></iframe>
-                </div>
-              </div>
-            </div>
-          </div>
-
-        </div>
-      </div>
-
+      <section id='carsIntro'>
+        <h1 className='text-center' style={{marginTop:100, color:'#fff'}}>CarFeverTv</h1>
+      </section>
+      <section id='carsCalendar'></section>
+      <section id='carsShows'></section>
+      <section id='carsVote'></section>
     </div>
   );
-}
+};
 
 export default Cars;
