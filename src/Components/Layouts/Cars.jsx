453707d4fe365539e9755caeec66d1df78f6b06a
Jon Manock <jonmanock@Jons-MacBook-Pro.local>
2019-02-28 04:28:26 -0500

    working on new layout

--- src/Components/Layouts/Cars.jsx
+++ src/Components/Layouts/Cars.jsx
@@ -6,6 +6,12 @@ const Cars = () =>{
     <div className='container-fluid'>
       <section id='carsIntro'>
         <h1 className='text-center' style={{marginTop:100, color:'#fff'}}>CarFeverTv</h1>
+        <div className='card'>
+          <div className='card-body'>
+            <p className='card-text text-center'>This is the place for all things CARS!</p>
+            <p className='card-text text-center'></p>
+          </div>
+        </div>
       </section>
       <section id='carsCalendar'></section>
       <section id='carsShows'></section>
