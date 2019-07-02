ec935c0c4b8f86e40bb91a0734bb42768c530de4
Jon Manock <jonmanock@Jons-MacBook-Pro.local>
2019-07-02 17:08:10 -0400

    next

--- src/Components/Navbar/Footer.jsx
+++ src/Components/Navbar/Footer.jsx
@@ -3,16 +3,32 @@ import React,{Component} from 'react';
 export default class Footer extends Component{
   render(){
     return(
-      <footer className='page-footer' style={{backgroundColor:'#333',marginTop:100}}>
+      <footer className='page-footer' style={{color:'#fff',backgroundColor:'#333',marginTop:100}}>
         <ul className='nav justify-content-center'>
-          <li className='nav-link' style={{color:'#fff'}}>&copy;2019</li>
-          <li className='nav-item'><a href='#facebook' className='nav-link'>Facebook</a></li>
-          <li className='nav-item'><a href='#insta' className='nav-link'>Instagram</a></li>
-          <li className='nav-item'><a href='#twitter' className='nav-link'>Twitter</a></li>
-          <li className='nav-item'><a href='#email' className='nav-link'>Email</a></li>
-          <li className='nav-item'><a href='#youtube' className='nav-link'>Youtube</a></li>
+          <li className='nav-link'>&copy;2019 <span style={{color:'#fc0100'}}>CycleFeverTV</span></li>
+          <li className='nav-item'>
+       <a href='https://www.facebook.com/cftv.live19' className='nav-link'><i className='fab fa-facebook-f' style={styles.icons}></i></a>
+     </li>
+     <li className='nav-item'>
+        <a href='https://twitter.com/CycleFeverTV' className='nav-link'><i className='fab fa-twitter' style={styles.icons}></i></a>
+      </li>
+      <li className='nav-item'>
+        <a href='https://www.instagram.com/cyclefevertv/' className='nav-link'><i style={styles.icons} className='fab fa-instagram'></i></a>
+      </li>
+      <li className='nav-item'>
+        <a href='https://www.youtube.com/channel/UC5lBCpUunSuN3lCxxTfkunA' className='nav-link'><i className='fab fa-youtube' style={styles.icons}></i></a>
+      </li>
+      <li className='nav-item'>
+        <a href='mailto:cftv@email.com' className='nav-link'><i className='fas fa-envelope' style={styles.icons}></i></a>
+      </li>
         </ul>
       </footer>
     );
+  }
+}
+const styles = {
+  icons:{
+    color:'#82b1ff',
+    fontSize:'1.3em'
   }
 }
